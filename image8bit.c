6e8353a31b576c43e6b54c4375ba13d2ac4a53dd
jpapinto <jpapinto@ua.pt>
2023-11-22 19:11:59 +0000

    ImagemMatchSubImage - Sem crop()

--- image8bit.c
+++ image8bit.c
@@ -609,22 +609,14 @@ int ImageMatchSubImage(Image img1, int x, int y, Image img2) { ///
   assert (ImageValidPos(img1, x, y));
   // Insert your code here!
 
-  printf("Value de x e y: %d %d \n", x,y);
-  Image subimg1 = ImageCrop(img1,x,y,img2->width,img2->height); // Nota: ImageCrop() /// Crop a rectangular subimage from img.
-
-  if(subimg1 == NULL){
-    perror("Error croping a rectangular subimage from img1.");
-    return EXIT_FAILURE;
+  for (int i = 0; i < img2->width; i++) {
+    for (int j = 0; j < img2->height; j++) {
+      if(ImageGetPixel(img1,x+i,y+j) != ImageGetPixel(img2,i,j) ) return 0;
+    }
   }
-
-  // Loop through all pixels and apply the mirror
-  for (size_t idx = 0; idx < img2->width * img2->height; idx++ ) {
-    if( subimg1->pixel[idx] != img2->pixel[idx] ) return 0;
-  }
-
-  free(subimg1);
 
   return 1;
+
 
 }
 
@@ -639,7 +631,6 @@ int ImageLocateSubImage(Image img1, int* px, int* py, Image img2) { ///
 
   for( int x=0; x <= img1->width - img2->width; x++) {
     for( int y=0; y <= img1->height - img2->height; y++) {
-//      printf("Valor de x e y: %d %d \n",x,y);
       if(ImageMatchSubImage(img1,x,y,img2)) { // Returns 1 (true) if img2 matches subimage of img1 at pos (x, y)
         *px = x;
         *py = y;
