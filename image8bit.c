f5a86aab896f6b6469ab7a66feaf8ef0c2762eab
jpapinto <jpapinto@ua.pt>
2023-11-22 18:29:37 +0000

    Version1 - ImageLocateSubImage()

--- image8bit.c
+++ image8bit.c
@@ -609,7 +609,9 @@ int ImageMatchSubImage(Image img1, int x, int y, Image img2) { ///
   assert (ImageValidPos(img1, x, y));
   // Insert your code here!
 
+  printf("Value de x e y: %d %d \n", x,y);
   Image subimg1 = ImageCrop(img1,x,y,img2->width,img2->height); // Nota: ImageCrop() /// Crop a rectangular subimage from img.
+
   if(subimg1 == NULL){
     perror("Error croping a rectangular subimage from img1.");
     return EXIT_FAILURE;
@@ -619,6 +621,8 @@ int ImageMatchSubImage(Image img1, int x, int y, Image img2) { ///
   for (size_t idx = 0; idx < img2->width * img2->height; idx++ ) {
     if( subimg1->pixel[idx] != img2->pixel[idx] ) return 0;
   }
+
+  free(subimg1);
 
   return 1;
 
@@ -633,8 +637,9 @@ int ImageLocateSubImage(Image img1, int* px, int* py, Image img2) { ///
   assert (img2 != NULL);
   // Insert your code here!
 
-  for( int x=0; x <= img1->width; x++) {
-    for( int y=0; y <= img1->width; y++) {
+  for( int x=0; x < img1->width - img2->width; x++) {
+    for( int y=0; y < img1->height - img2->height; y++) {
+//      printf("Valor de x e y: %d %d \n",x,y);
       if(ImageMatchSubImage(img1,x,y,img2)) { // Returns 1 (true) if img2 matches subimage of img1 at pos (x, y)
         *px = x;
         *py = y;
---
+++ versoes_LocateSubImage.c
@@ -0,0 +1,56 @@
+//
+// Created by joao on 11/22/23.
+//
+
+// Version 1
+
+int ImageMatchSubImage(Image img1, int x, int y, Image img2) { ///
+  assert (img1 != NULL);
+  assert (img2 != NULL);
+  assert (ImageValidPos(img1, x, y));
+  // Insert your code here!
+
+  printf("Value de x e y: %d %d \n", x,y);
+  Image subimg1 = ImageCrop(img1,x,y,img2->width,img2->height); // Nota: ImageCrop() /// Crop a rectangular subimage from img.
+
+  if(subimg1 == NULL){
+    perror("Error croping a rectangular subimage from img1.");
+    return EXIT_FAILURE;
+  }
+
+  // Loop through all pixels and apply the mirror
+  for (size_t idx = 0; idx < img2->width * img2->height; idx++ ) {
+    if( subimg1->pixel[idx] != img2->pixel[idx] ) return 0;
+  }
+
+  free(subimg1);
+
+  return 1;
+
+}
+
+/// Locate a subimage inside another image.
+/// Searches for img2 inside img1.
+/// If a match is found, returns 1 and matching position is set in vars (*px, *py).
+/// If no match is found, returns 0 and (*px, *py) are left untouched.
+int ImageLocateSubImage(Image img1, int* px, int* py, Image img2) { ///
+  assert (img1 != NULL);
+  assert (img2 != NULL);
+  // Insert your code here!
+
+  for( int x=0; x < img1->width - img2->width; x++) {
+    for( int y=0; y < img1->height - img2->height; y++) {
+//      printf("Valor de x e y: %d %d \n",x,y);
+      if(ImageMatchSubImage(img1,x,y,img2)) { // Returns 1 (true) if img2 matches subimage of img1 at pos (x, y)
+        *px = x;
+        *py = y;
+        return 1;
+      }
+    }
+  }
+
+  return 0;
+}
+
+// Version 2
+
